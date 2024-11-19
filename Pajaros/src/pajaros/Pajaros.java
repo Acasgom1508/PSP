30a97a7c3493619c14bcb3eba5ee2afc2142aaed
Acasgom1508 <166312439+Acasgom1508@users.noreply.github.com>
2024-11-19 11:16:32 +0100

    Creado el proyecto de pajaros

---
+++ Pajaros/src/pajaros/Pajaros.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pajaros;
+
+/**
+ *
+ * @author DAM_M
+ */
+public class Pajaros {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
