f1dbec83cf107ce77619219bea21f1db71635a32
Acasgom1508 <166312439+Acasgom1508@users.noreply.github.com>
2024-11-19 09:49:01 +0100

    Terminado lenguaje y creado colaborar

--- Lenguaje/src/lenguaje/Lenguaje.java
+++ Lenguaje/src/lenguaje/Lenguaje.java
@@ -4,6 +4,10 @@
  */
 package lenguaje;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 /**
  *
  * @author DAM_M
@@ -14,7 +18,28 @@ public class Lenguaje {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //Creo el array de cadena
+        String[] cadena = new String[Integer.parseInt(args[1])];
+
+        //Genero las palabras aleatorias
+        for (int i = 0; i < cadena.length; i++) {
+            StringBuilder sb = new StringBuilder();
+            for (int j = 0; j < 5; j++) {
+                sb.append((char) (Math.random() * 26 + 'a'));
+            }
+            cadena[i] = "Escrito por " + args[0] + ": " + sb.toString();
+        }
+
+        //Escribo el codigo en el archivo con nombre en args[2] usando bufferedWritter
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(args[2]))) {
+            //Escribo cada cadena en una linea diferente
+            for (String linea : cadena) {
+                bw.write(linea);
+                bw.newLine();
+            }
+        } catch (IOException e) { //capturo la error de entrada y salida
+            System.err.println("Error al escribir el archivo: " + e.getMessage());
+        };
     }
     
 }
