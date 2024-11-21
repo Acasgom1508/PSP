053799821a073694dc88bb7ab1d7ca40be8e61f0
Acasgom1508 <166312439+Acasgom1508@users.noreply.github.com>
2024-11-21 20:01:46 +0100

    Terminado bar cañas

--- BarCanas/src/barcanas/BarCanas.java
+++ BarCanas/src/barcanas/BarCanas.java
@@ -1,20 +1,45 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package barcanas;
 
-/**
- *
- * @author DAM_M
- */
 public class BarCanas {
+    private final int totalCañas;
+    private int cañasServidas;
+    private boolean barrileNecesitaReposicion;
+    private final Object lock = new Object();
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        
+    public BarCanas(int nCañas) {
+        this.totalCañas = nCañas;
+        this.cañasServidas = 0;
+        this.barrileNecesitaReposicion = false;
     }
-    
+
+    public void tomarCaña(int id) throws InterruptedException {
+        synchronized (lock) {
+            // Si el barril está agotado, intentar reponerlo inmediatamente
+            if (barrileNecesitaReposicion) {
+                reponer();
+            }
+
+            // Servir caña
+            cañasServidas++;
+            System.out.println("Sirviendo caña " + cañasServidas + " al cliente " + id + "...");
+            Thread.sleep(500);
+
+            // Comprobar si el barril está vacío
+            if (cañasServidas >= totalCañas) {
+                barrileNecesitaReposicion = true;
+                System.out.println("Barril agotado. Reponiendo...");
+            }
+        }
+    }
+
+    public void reponer() throws InterruptedException {
+        synchronized (lock) {
+            if (barrileNecesitaReposicion) {
+                cañasServidas = 0;
+                barrileNecesitaReposicion = false;
+                Thread.sleep(1500);
+                System.out.println("Barril reemplazado.");
+            }
+        }
+    }
 }
